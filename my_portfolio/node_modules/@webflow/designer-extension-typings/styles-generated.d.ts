f344957873d8a7aaea1aba30a7bf24b7e5c52c71
sanspot <>
2025-10-06 18:29:08 +0200

    Uploading starter project

---
+++ my_portfolio/node_modules/@webflow/designer-extension-typings/styles-generated.d.ts
@@ -0,0 +1,588 @@
+// This file was automatically generated. See designer-extensions docs.
+
+type PropertyMap = {
+  'accent-color'?: string | ColorVariable;
+  'align-content'?: string;
+  'align-items'?: string;
+  'align-self'?: string;
+  'animation-delay'?: string;
+  'animation-direction'?: string;
+  'animation-duration'?: string;
+  'animation-fill-mode'?: string;
+  'animation-iteration-count'?: string;
+  'animation-name'?: string;
+  'animation-play-state'?: string;
+  'animation-timing-function'?: string;
+  appearance?: string;
+  'backdrop-filter'?: string | SizeVariable;
+  'backface-visibility'?: string;
+  'background-attachment'?: string;
+  'background-blend-mode'?: string;
+  'background-clip'?: string;
+  'background-color'?: string | ColorVariable;
+  'background-image'?: string | ColorVariable;
+  'background-origin'?: string;
+  'background-position'?: string;
+  'background-position-x'?: string | SizeVariable;
+  'background-position-y'?: string | SizeVariable;
+  'background-repeat'?: string;
+  'background-size'?: string;
+  'block-size'?: string | SizeVariable;
+  'border-block-end-color'?: string | ColorVariable;
+  'border-block-end-style'?: string;
+  'border-block-end-width'?: string | SizeVariable;
+  'border-block-start-color'?: string | ColorVariable;
+  'border-block-start-style'?: string;
+  'border-block-start-width'?: string | SizeVariable;
+  'border-bottom-color'?: string | ColorVariable;
+  'border-bottom-left-radius'?: string | SizeVariable | PercentageVariable;
+  'border-bottom-right-radius'?: string | SizeVariable | PercentageVariable;
+  'border-bottom-style'?: string;
+  'border-bottom-width'?: string | SizeVariable;
+  'border-collapse'?: string;
+  'border-color'?: string | ColorVariable;
+  'border-end-end-radius'?: string | SizeVariable;
+  'border-end-start-radius'?: string | SizeVariable;
+  'border-image-outset'?: string | SizeVariable;
+  'border-image-repeat'?: string;
+  'border-image-slice'?: string;
+  'border-image-source'?: string;
+  'border-image-width'?: string | SizeVariable;
+  'border-inline-end-color'?: string | ColorVariable;
+  'border-inline-end-style'?: string;
+  'border-inline-end-width'?: string | SizeVariable;
+  'border-inline-start-color'?: string | ColorVariable;
+  'border-inline-start-style'?: string;
+  'border-inline-start-width'?: string | SizeVariable;
+  'border-left-color'?: string | ColorVariable;
+  'border-left-style'?: string;
+  'border-left-width'?: string | SizeVariable;
+  'border-radius'?: string | SizeVariable | PercentageVariable;
+  'border-right-color'?: string | ColorVariable;
+  'border-right-style'?: string;
+  'border-right-width'?: string | SizeVariable;
+  'border-start-end-radius'?: string | SizeVariable;
+  'border-start-start-radius'?: string | SizeVariable;
+  'border-top-color'?: string | ColorVariable;
+  'border-top-left-radius'?: string | SizeVariable | PercentageVariable;
+  'border-top-right-radius'?: string | SizeVariable | PercentageVariable;
+  'border-top-style'?: string;
+  'border-top-width'?: string | SizeVariable;
+  'border-width'?: string | SizeVariable;
+  bottom?: string | SizeVariable | PercentageVariable;
+  'box-shadow'?: string | SizeVariable;
+  'box-sizing'?: string;
+  'break-after'?: string;
+  'break-before'?: string;
+  'break-inside'?: string;
+  'caption-side'?: string;
+  'caret-color'?: string | ColorVariable;
+  clear?: string;
+  clip?: string;
+  'clip-path'?: string;
+  'clip-rule'?: string;
+  color?: string | ColorVariable;
+  'color-interpolation'?: string;
+  'color-interpolation-filters'?: string;
+  'column-count'?: string;
+  'column-gap'?: string | SizeVariable;
+  'column-rule-color'?: string | ColorVariable;
+  'column-rule-style'?: string;
+  'column-rule-width'?: string | SizeVariable;
+  'column-span'?: string;
+  'column-width'?: string | SizeVariable;
+  content?: string;
+  cursor?: string;
+  cx?: string;
+  cy?: string;
+  direction?: string;
+  display?: string;
+  'dominant-baseline'?: string;
+  'empty-cells'?: string;
+  fill?: string;
+  'fill-opacity'?: string;
+  'fill-rule'?: string;
+  filter?: string | SizeVariable;
+  'flex-basis'?: string | SizeVariable | PercentageVariable;
+  'flex-direction'?: string;
+  'flex-grow'?: string | NumberVariable;
+  'flex-shrink'?: string | NumberVariable;
+  'flex-wrap'?: string;
+  float?: string;
+  'flood-color'?: string | ColorVariable;
+  'flood-opacity'?: string;
+  'font-family'?: string | FontFamilyVariable;
+  'font-kerning'?: string;
+  'font-optical-sizing'?: string;
+  'font-size'?: string | SizeVariable | PercentageVariable;
+  'font-stretch'?: string;
+  'font-style'?: string;
+  'font-variant-alternates'?: string;
+  'font-variant-caps'?: string;
+  'font-variant-east-asian'?: string;
+  'font-variant-ligatures'?: string;
+  'font-variant-numeric'?: string;
+  'font-weight'?: string | NumberVariable;
+  'grid-auto-columns'?: string;
+  'grid-auto-flow'?: string;
+  'grid-auto-rows'?: string;
+  'grid-column-end'?: string;
+  'grid-column-gap'?: string | SizeVariable | PercentageVariable;
+  'grid-column-start'?: string;
+  'grid-row-end'?: string;
+  'grid-row-gap'?: string | SizeVariable | PercentageVariable;
+  'grid-row-start'?: string;
+  'grid-template-areas'?: string;
+  'grid-template-columns'?: string;
+  'grid-template-rows'?: string;
+  height?: string | SizeVariable | PercentageVariable;
+  'image-orientation'?: string;
+  'image-rendering'?: string;
+  'inline-size'?: string | SizeVariable;
+  'inset-block-end'?: string | SizeVariable;
+  'inset-block-start'?: string | SizeVariable;
+  'inset-inline-end'?: string | SizeVariable;
+  'inset-inline-start'?: string | SizeVariable;
+  isolation?: string;
+  'justify-content'?: string;
+  'justify-items'?: string;
+  'justify-self'?: string;
+  left?: string | SizeVariable | PercentageVariable;
+  'letter-spacing'?: string | SizeVariable;
+  'lighting-color'?: string | ColorVariable;
+  'line-break'?: string;
+  'line-height'?: string | SizeVariable | NumberVariable | PercentageVariable;
+  'list-style-image'?: string;
+  'list-style-position'?: string;
+  'list-style-type'?: string;
+  'margin-block-end'?: string | SizeVariable;
+  'margin-block-start'?: string | SizeVariable;
+  'margin-bottom'?: string | SizeVariable | PercentageVariable;
+  'margin-inline-end'?: string | SizeVariable;
+  'margin-inline-start'?: string | SizeVariable;
+  'margin-left'?: string | SizeVariable | PercentageVariable;
+  'margin-right'?: string | SizeVariable | PercentageVariable;
+  'margin-top'?: string | SizeVariable | PercentageVariable;
+  'marker-end'?: string;
+  'marker-mid'?: string;
+  'marker-start'?: string;
+  'mask-type'?: string;
+  'max-block-size'?: string | SizeVariable;
+  'max-height'?: string | SizeVariable | PercentageVariable;
+  'max-inline-size'?: string | SizeVariable;
+  'max-width'?: string | SizeVariable | PercentageVariable;
+  'min-block-size'?: string | SizeVariable;
+  'min-height'?: string | SizeVariable | PercentageVariable;
+  'min-inline-size'?: string | SizeVariable;
+  'min-width'?: string | SizeVariable | PercentageVariable;
+  'mix-blend-mode'?: string;
+  'object-fit'?: string;
+  'object-position'?: string;
+  'offset-anchor'?: string;
+  'offset-distance'?: string | SizeVariable;
+  'offset-path'?: string;
+  'offset-rotate'?: string;
+  opacity?: string | NumberVariable | PercentageVariable;
+  order?: string;
+  'outline-color'?: string | ColorVariable;
+  'outline-offset'?: string | SizeVariable;
+  'outline-style'?: string;
+  'outline-width'?: string | SizeVariable;
+  'overflow-wrap'?: string;
+  'overflow-x'?: string;
+  'overflow-y'?: string;
+  'overscroll-behavior-block'?: string;
+  'overscroll-behavior-inline'?: string;
+  'padding-block-end'?: string | SizeVariable;
+  'padding-block-start'?: string | SizeVariable;
+  'padding-bottom'?: string | SizeVariable | PercentageVariable;
+  'padding-inline-end'?: string | SizeVariable;
+  'padding-inline-start'?: string | SizeVariable;
+  'padding-left'?: string | SizeVariable | PercentageVariable;
+  'padding-right'?: string | SizeVariable | PercentageVariable;
+  'padding-top'?: string | SizeVariable | PercentageVariable;
+  'paint-order'?: string;
+  perspective?: string | SizeVariable;
+  'perspective-origin'?: string;
+  'pointer-events'?: string;
+  position?: string;
+  r?: string | SizeVariable;
+  resize?: string;
+  right?: string | SizeVariable | PercentageVariable;
+  rotate?: string;
+  'row-gap'?: string | SizeVariable;
+  rx?: string | SizeVariable;
+  ry?: string | SizeVariable;
+  scale?: string;
+  'scroll-behavior'?: string;
+  'scroll-margin-block-end'?: string | SizeVariable;
+  'scroll-margin-block-start'?: string | SizeVariable;
+  'scroll-margin-inline-end'?: string | SizeVariable;
+  'scroll-margin-inline-start'?: string | SizeVariable;
+  'scroll-padding-block-end'?: string | SizeVariable;
+  'scroll-padding-block-start'?: string | SizeVariable;
+  'scroll-padding-inline-end'?: string | SizeVariable;
+  'scroll-padding-inline-start'?: string | SizeVariable;
+  'shape-image-threshold'?: string;
+  'shape-margin'?: string | SizeVariable;
+  'shape-outside'?: string;
+  'shape-rendering'?: string;
+  'stop-color'?: string | ColorVariable;
+  'stop-opacity'?: string;
+  stroke?: string | ColorVariable;
+  'stroke-dasharray'?: string;
+  'stroke-dashoffset'?: string | SizeVariable;
+  'stroke-linecap'?: string;
+  'stroke-linejoin'?: string;
+  'stroke-miterlimit'?: string;
+  'stroke-opacity'?: string;
+  'stroke-width'?: string | SizeVariable;
+  'tab-size'?: string | SizeVariable;
+  'table-layout'?: string;
+  'text-align'?: string;
+  'text-align-last'?: string;
+  'text-anchor'?: string;
+  'text-decoration'?: string;
+  'text-decoration-color'?: string | ColorVariable;
+  'text-decoration-line'?: string;
+  'text-decoration-skip-ink'?: string;
+  'text-decoration-style'?: string;
+  'text-emphasis-color'?: string | ColorVariable;
+  'text-emphasis-position'?: string;
+  'text-emphasis-style'?: string;
+  'text-indent'?: string | SizeVariable | PercentageVariable;
+  'text-overflow'?: string;
+  'text-rendering'?: string;
+  'text-shadow'?: string | SizeVariable;
+  'text-transform'?: string;
+  'text-underline-position'?: string;
+  top?: string | SizeVariable | PercentageVariable;
+  'touch-action'?: string;
+  transform?: string;
+  'transform-origin'?: string;
+  'transform-style'?: string;
+  'transition-delay'?: string;
+  'transition-duration'?: string;
+  'transition-property'?: string;
+  'transition-timing-function'?: string;
+  translate?: string | SizeVariable;
+  'unicode-bidi'?: string;
+  'vector-effect'?: string;
+  'vertical-align'?: string;
+  visibility?: string;
+  'white-space'?: string;
+  width?: string | SizeVariable | PercentageVariable;
+  'will-change'?: string;
+  'word-break'?: string;
+  'word-spacing'?: string | SizeVariable;
+  'writing-mode'?: string;
+  x?: string | SizeVariable;
+  y?: string | SizeVariable;
+  'z-index'?: string | NumberVariable;
+  '-webkit-line-clamp'?: string;
+  '-webkit-text-fill-color'?: string | ColorVariable;
+  '-webkit-text-stroke-color'?: string | ColorVariable;
+  '-webkit-text-stroke-width'?: string | SizeVariable;
+};
+
+type StyleProperty =
+  | 'accent-color'
+  | 'align-content'
+  | 'align-items'
+  | 'align-self'
+  | 'animation-delay'
+  | 'animation-direction'
+  | 'animation-duration'
+  | 'animation-fill-mode'
+  | 'animation-iteration-count'
+  | 'animation-name'
+  | 'animation-play-state'
+  | 'animation-timing-function'
+  | 'appearance'
+  | 'backdrop-filter'
+  | 'backface-visibility'
+  | 'background-attachment'
+  | 'background-blend-mode'
+  | 'background-clip'
+  | 'background-color'
+  | 'background-image'
+  | 'background-origin'
+  | 'background-position'
+  | 'background-position-x'
+  | 'background-position-y'
+  | 'background-repeat'
+  | 'background-size'
+  | 'block-size'
+  | 'border-block-end-color'
+  | 'border-block-end-style'
+  | 'border-block-end-width'
+  | 'border-block-start-color'
+  | 'border-block-start-style'
+  | 'border-block-start-width'
+  | 'border-bottom-color'
+  | 'border-bottom-left-radius'
+  | 'border-bottom-right-radius'
+  | 'border-bottom-style'
+  | 'border-bottom-width'
+  | 'border-collapse'
+  | 'border-color'
+  | 'border-end-end-radius'
+  | 'border-end-start-radius'
+  | 'border-image-outset'
+  | 'border-image-repeat'
+  | 'border-image-slice'
+  | 'border-image-source'
+  | 'border-image-width'
+  | 'border-inline-end-color'
+  | 'border-inline-end-style'
+  | 'border-inline-end-width'
+  | 'border-inline-start-color'
+  | 'border-inline-start-style'
+  | 'border-inline-start-width'
+  | 'border-left-color'
+  | 'border-left-style'
+  | 'border-left-width'
+  | 'border-radius'
+  | 'border-right-color'
+  | 'border-right-style'
+  | 'border-right-width'
+  | 'border-start-end-radius'
+  | 'border-start-start-radius'
+  | 'border-top-color'
+  | 'border-top-left-radius'
+  | 'border-top-right-radius'
+  | 'border-top-style'
+  | 'border-top-width'
+  | 'border-width'
+  | 'bottom'
+  | 'box-shadow'
+  | 'box-sizing'
+  | 'break-after'
+  | 'break-before'
+  | 'break-inside'
+  | 'caption-side'
+  | 'caret-color'
+  | 'clear'
+  | 'clip'
+  | 'clip-path'
+  | 'clip-rule'
+  | 'color'
+  | 'color-interpolation'
+  | 'color-interpolation-filters'
+  | 'column-count'
+  | 'column-gap'
+  | 'column-rule-color'
+  | 'column-rule-style'
+  | 'column-rule-width'
+  | 'column-span'
+  | 'column-width'
+  | 'content'
+  | 'cursor'
+  | 'cx'
+  | 'cy'
+  | 'direction'
+  | 'display'
+  | 'dominant-baseline'
+  | 'empty-cells'
+  | 'fill'
+  | 'fill-opacity'
+  | 'fill-rule'
+  | 'filter'
+  | 'flex-basis'
+  | 'flex-direction'
+  | 'flex-grow'
+  | 'flex-shrink'
+  | 'flex-wrap'
+  | 'float'
+  | 'flood-color'
+  | 'flood-opacity'
+  | 'font-family'
+  | 'font-kerning'
+  | 'font-optical-sizing'
+  | 'font-size'
+  | 'font-stretch'
+  | 'font-style'
+  | 'font-variant-alternates'
+  | 'font-variant-caps'
+  | 'font-variant-east-asian'
+  | 'font-variant-ligatures'
+  | 'font-variant-numeric'
+  | 'font-weight'
+  | 'grid-auto-columns'
+  | 'grid-auto-flow'
+  | 'grid-auto-rows'
+  | 'grid-column-end'
+  | 'grid-column-gap'
+  | 'grid-column-start'
+  | 'grid-row-end'
+  | 'grid-row-gap'
+  | 'grid-row-start'
+  | 'grid-template-areas'
+  | 'grid-template-columns'
+  | 'grid-template-rows'
+  | 'height'
+  | 'image-orientation'
+  | 'image-rendering'
+  | 'inline-size'
+  | 'inset-block-end'
+  | 'inset-block-start'
+  | 'inset-inline-end'
+  | 'inset-inline-start'
+  | 'isolation'
+  | 'justify-content'
+  | 'justify-items'
+  | 'justify-self'
+  | 'left'
+  | 'letter-spacing'
+  | 'lighting-color'
+  | 'line-break'
+  | 'line-height'
+  | 'list-style-image'
+  | 'list-style-position'
+  | 'list-style-type'
+  | 'margin-block-end'
+  | 'margin-block-start'
+  | 'margin-bottom'
+  | 'margin-inline-end'
+  | 'margin-inline-start'
+  | 'margin-left'
+  | 'margin-right'
+  | 'margin-top'
+  | 'marker-end'
+  | 'marker-mid'
+  | 'marker-start'
+  | 'mask-type'
+  | 'max-block-size'
+  | 'max-height'
+  | 'max-inline-size'
+  | 'max-width'
+  | 'min-block-size'
+  | 'min-height'
+  | 'min-inline-size'
+  | 'min-width'
+  | 'mix-blend-mode'
+  | 'object-fit'
+  | 'object-position'
+  | 'offset-anchor'
+  | 'offset-distance'
+  | 'offset-path'
+  | 'offset-rotate'
+  | 'opacity'
+  | 'order'
+  | 'outline-color'
+  | 'outline-offset'
+  | 'outline-style'
+  | 'outline-width'
+  | 'overflow-wrap'
+  | 'overflow-x'
+  | 'overflow-y'
+  | 'overscroll-behavior-block'
+  | 'overscroll-behavior-inline'
+  | 'padding-block-end'
+  | 'padding-block-start'
+  | 'padding-bottom'
+  | 'padding-inline-end'
+  | 'padding-inline-start'
+  | 'padding-left'
+  | 'padding-right'
+  | 'padding-top'
+  | 'paint-order'
+  | 'perspective'
+  | 'perspective-origin'
+  | 'pointer-events'
+  | 'position'
+  | 'r'
+  | 'resize'
+  | 'right'
+  | 'rotate'
+  | 'row-gap'
+  | 'rx'
+  | 'ry'
+  | 'scale'
+  | 'scroll-behavior'
+  | 'scroll-margin-block-end'
+  | 'scroll-margin-block-start'
+  | 'scroll-margin-inline-end'
+  | 'scroll-margin-inline-start'
+  | 'scroll-padding-block-end'
+  | 'scroll-padding-block-start'
+  | 'scroll-padding-inline-end'
+  | 'scroll-padding-inline-start'
+  | 'shape-image-threshold'
+  | 'shape-margin'
+  | 'shape-outside'
+  | 'shape-rendering'
+  | 'stop-color'
+  | 'stop-opacity'
+  | 'stroke'
+  | 'stroke-dasharray'
+  | 'stroke-dashoffset'
+  | 'stroke-linecap'
+  | 'stroke-linejoin'
+  | 'stroke-miterlimit'
+  | 'stroke-opacity'
+  | 'stroke-width'
+  | 'tab-size'
+  | 'table-layout'
+  | 'text-align'
+  | 'text-align-last'
+  | 'text-anchor'
+  | 'text-decoration'
+  | 'text-decoration-color'
+  | 'text-decoration-line'
+  | 'text-decoration-skip-ink'
+  | 'text-decoration-style'
+  | 'text-emphasis-color'
+  | 'text-emphasis-position'
+  | 'text-emphasis-style'
+  | 'text-indent'
+  | 'text-overflow'
+  | 'text-rendering'
+  | 'text-shadow'
+  | 'text-transform'
+  | 'text-underline-position'
+  | 'top'
+  | 'touch-action'
+  | 'transform'
+  | 'transform-origin'
+  | 'transform-style'
+  | 'transition-delay'
+  | 'transition-duration'
+  | 'transition-property'
+  | 'transition-timing-function'
+  | 'translate'
+  | 'unicode-bidi'
+  | 'vector-effect'
+  | 'vertical-align'
+  | 'visibility'
+  | 'white-space'
+  | 'width'
+  | 'will-change'
+  | 'word-break'
+  | 'word-spacing'
+  | 'writing-mode'
+  | 'x'
+  | 'y'
+  | 'z-index'
+  | '-webkit-line-clamp'
+  | '-webkit-text-fill-color'
+  | '-webkit-text-stroke-color'
+  | '-webkit-text-stroke-width';
+
+type PseudoStateKey =
+  | 'noPseudo'
+  | 'nth-child(odd)'
+  | 'nth-child(even)'
+  | 'first-child'
+  | 'last-child'
+  | 'hover'
+  | 'active'
+  | 'pressed'
+  | 'visited'
+  | 'focus'
+  | 'focus-visible'
+  | 'focus-within'
+  | 'placeholder'
+  | 'empty'
+  | 'before'
+  | 'after';
---
+++ my_portfolio/public/index.js
@@ -0,0 +1,17 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+document.getElementById("lorem").onsubmit = (event) => __awaiter(this, void 0, void 0, function* () {
+    event.preventDefault();
+    const el = yield webflow.getSelectedElement();
+    if (el && el.textContent) {
+        yield el.setTextContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
+            "eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+    }
+});
